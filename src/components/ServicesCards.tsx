c1c2c8821c0debe30badf85360294e51ba4d0ab7
Harshit Yadav <2105891@kiit.ac.in>
2024-03-14 22:53:01 +0530

    feat: services card update

--- src/components/ServicesCards.tsx
+++ src/components/ServicesCards.tsx
@@ -1,4 +1,5 @@
 
+import { Link } from 'react-router-dom'
 import './ServicesCard.css'
 const ServicesCards = () => {
     return (
@@ -11,11 +12,13 @@ const ServicesCards = () => {
                             <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Health and Nutrition</h1>
                             <p className="text-left font-light  text-sm">
 
-                                Nature’s Club combats Odisha's child malnutrition (45%), focusing on hard-to-reach areas. Initially centered on children and mothers, it now emphasizes adolescent health, particularly of future mothers.
+                                Despite modern advancements, the timeless adage "Health is Wealth" remains pertinent.
                             </p>
                         </div>
                         <button className='md:py-2 py-1 px-2 md:px-4 bg-white text-black rounded-[4px] text-sm lg:text-base mb-12 w-1/2'>
-                            Learn More
+                            <Link to='/services'>
+                                Learn More
+                            </Link>
                         </button>
                     </div>
                 </div>
@@ -25,14 +28,16 @@ const ServicesCards = () => {
                     <div className="w-full p-4 h-full  text-white absolute z-20  flex flex-col justify-between">
                         <div className='w-full'>
 
-                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Project Anand</h1>
+                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Ecology and Environment</h1>
                             <p className="text-left font-light md:text-base text-sm">
 
-                                Project Anand supports children affected by HIV/AIDS in Kendrapara with nutrition, education, and hygiene kits, emphasizing care and reducing stigma.
+                                Nature’s Club partners with TERI's Light a Billion Lives program, installing solar lanterns and mobile charging facilities in unelectrified villages of Kendrapara district.
                             </p>
                         </div>
                         <button className='md:py-2 py-1 px-2 md:px-4 bg-white text-black rounded-[4px] text-sm lg:text-base mb-12 w-1/2'>
-                            Learn More
+                            <Link to='/services'>
+                                Learn More
+                            </Link>
                         </button>
                     </div>
                 </div>
@@ -43,13 +48,15 @@ const ServicesCards = () => {
                     <div className="w-full p-4 h-full  text-white absolute z-20  flex flex-col justify-between">
                         <div className='w-full'>
 
-                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Targeted Intervention Project</h1>
+                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Disaster Risk Reduction</h1>
                             <p className="text-left font-light md:text-base text-sm">
-                                Nature’s Club's TI project in Keonjhar, since March 2009, engages 200 FSWs and 150 MSWs, achieving behavior change in safe sex practices.
+                                Resilient children and resilient communities are the goal of CCDRR.
                             </p>
                         </div>
                         <button className='md:py-2 py-1 px-2 md:px-4 bg-white text-black rounded-[4px] text-sm lg:text-base mb-12 w-1/2'>
-                            Learn More
+                            <Link to='/services'>
+                                Learn More
+                            </Link>
                         </button>
                     </div>
                 </div>
@@ -60,13 +67,15 @@ const ServicesCards = () => {
                     <div className="w-full p-4 h-full  text-white absolute z-20  flex flex-col justify-between">
                         <div className='w-full'>
 
-                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Clean energy solution</h1>
+                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Climate change</h1>
                             <p className="text-left font-light md:text-base text-sm">
-                                Nature’s Club partners with TERI in the Light a Billion Lives program since 2009, bringing green energy to unelectrified villages in Kendrapara with 22 solar lantern units and 27 solar mobile charging facilities.
+                                Climate change is increasingly threatening lives, habitat and livelihood, the most in the circle of Bay of Bengal.
                             </p>
                         </div>
                         <button className='md:py-2 py-1 px-2 md:px-4 bg-white text-black rounded-[4px] text-sm lg:text-base mb-12 w-1/2'>
-                            Learn More
+                            <Link to='/services'>
+                                Learn More
+                            </Link>
                         </button>
                     </div>
                 </div>
@@ -76,13 +85,15 @@ const ServicesCards = () => {
                 <div className="flex flex-col lg:h-[400px] h-[300px] md:h-96 bg-img bg-img-women  rounded-2.5xl relative">
                     <div className="w-full p-4 h-full  text-white absolute z-20  flex flex-col justify-between">
                         <div className='w-full'>
-                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Resilient Livelihood</h1>
+                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Child Rights and Child protection</h1>
                             <p className="text-left font-light md:text-base text-sm">
-                                Climate change threatens Bay of Bengal communities. Bhitarkanika Wildlife Sanctuary prioritizes livelihood protection in its agrarian, one-crop rain-fed paddy, and fishing-dependent area.
+                                The candle of enlightenment, which was lightened by Nature's Club with support from UNICEF
                             </p>
                         </div>
                         <button className='md:py-2 py-1 px-2 md:px-4 bg-white text-black rounded-[4px] text-sm lg:text-base mb-12 w-1/2'>
-                            Learn More
+                            <Link to='/services'>
+                                Learn More
+                            </Link>
                         </button>
 
                     </div>
@@ -94,13 +105,15 @@ const ServicesCards = () => {
                     <div className="w-full p-4 h-full  text-white absolute z-20  flex flex-col justify-between">
                         <div className='w-full'>
 
-                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Child rights and child protection</h1>
+                            <h1 className="text-left mt-8 text-base md:text-2xl font-bold mb-2">Water, Sanitation and Hygiene</h1>
                             <p className="text-left font-light md:text-base text-sm">
-                                Nature’s Club, supported by UNICEF, ignited enlightenment in the backward G.P. Baradia, sparking a ripple effect for girls' rights.
+                                Ensuring access to clean water and sanitation for all communities is essential for public health and sustainable development.
                             </p>
                         </div>
                         <button className='md:py-2 py-1 px-2 md:px-4 bg-white text-black rounded-[4px] text-sm lg:text-base mb-12 w-1/2'>
-                            Learn More
+                            <Link to='/services'>
+                                Learn More
+                            </Link>
                         </button>
                     </div>
                 </div>
