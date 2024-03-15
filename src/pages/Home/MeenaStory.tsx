af0efd86f393fb90572ce5d693253b1db4718b57
Harshit Yadav <2105891@kiit.ac.in>
2024-03-15 20:47:54 +0530

    fix: removed extra line from MeenaStory

--- src/pages/Home/MeenaStory.tsx
+++ src/pages/Home/MeenaStory.tsx
@@ -1,5 +1,5 @@
 
-import { IoIosLink } from "react-icons/io";
+// import { IoIosLink } from "react-icons/io";
 
 const MeenaStory = () => {
 
@@ -10,12 +10,14 @@ const MeenaStory = () => {
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">MEENA'S STORY
 
                     </h1>
-                    <p className="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                    <p className="mb-8 leading-relaxed">
+
+                    </p>
                     <div className="flex justify-center">
-                        <button className="inline-flex text-white bg-fern_green border-0 py-2 px-6 focus:outline-none  rounded-4xl lg:text-lg items-center gap-4 text-base">
+                        {/* <button className="inline-flex text-white bg-fern_green border-0 py-2 px-6 focus:outline-none  rounded-4xl lg:text-lg items-center gap-4 text-base">
                             <IoIosLink size={24} className="w-8 h-8" />
                             Full Video
-                        </button>
+                        </button> */}
 
                     </div>
                 </div>
